a2964391b7fd74e22c36ef3a52d1dfcde0bf3ed7
GlukoLyoFF <glukolyoff@gmail.com>
2021-02-20 15:08:09 +0000

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0) {
+    let res = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < res) {
+        res = array[i];
+      };
+    };
+    return res;
+  };
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0) {
+    let res = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > res) {
+        res = array[i];
+      };
+    };
+    return res;
+  };
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    };
+    return sum / array.length;
+  };
   return 0;
 }
